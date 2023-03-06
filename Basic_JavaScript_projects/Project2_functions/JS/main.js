371bacec46204b8641f164234b0ae17b35c42316
Andrei <atrubaa@gmail.com>
2023-03-06 12:11:36 -0800

    added math function

--- Basic_JavaScript_projects/Project2_functions/JS/main.js
+++ Basic_JavaScript_projects/Project2_functions/JS/main.js
@@ -7,3 +7,8 @@ function MyFunction(){
     sentense +="JavaScript right now";
     document.getElementById("concatenate").innerHTML=sentense; // find a p element of HTML and give it a value of string sentense.
 }
+
+function Addition(){
+    var math = 2+2;
+    document.getElementById("math").innerHTML="2+2=" + math;
+}
