8e32175be463976985c98a2d906ed49d66e751cb
Andrei <atrubaa@gmail.com>
2023-03-06 09:57:55 -0800

    add a buttom with function

--- Basic_JavaScript_projects/Project1_expressions_alert/JS/JavaScript_File1.js
+++ Basic_JavaScript_projects/Project1_expressions_alert/JS/JavaScript_File1.js
@@ -1,3 +1,7 @@
 var Text = "You\' are fucking cockroach: \"I'll kill you like a freaking dog!\"", Dad="Bobby", Mother="Julia";
 var Text2 = " Sorry, it is a bad day today";
-document.write(12*5421,3);
+
+function OnCLick(){
+    var str = "Here is my buttom text";
+    document.getElementById("Buttom_text").innerHTML=str;
+}
