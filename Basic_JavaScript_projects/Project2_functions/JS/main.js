538dc9a8b30672e65e59cbb365ecfca22069b4fb
Andrei <atrubaa@gmail.com>
2023-03-06 11:11:21 -0800

    added strings function

--- Basic_JavaScript_projects/Project2_functions/JS/main.js
+++ Basic_JavaScript_projects/Project2_functions/JS/main.js
@@ -1,3 +1,9 @@
 function displayDate(){
     document.getElementById("demo").innerHTML=Date();
 }
+
+function MyFunction(){
+    var sentense = "I am learning";
+    sentense +="JavaScript right now";
+    document.getElementById("concatenate").innerHTML=sentense;
+}
