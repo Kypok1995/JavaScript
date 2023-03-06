3db6fee551f89052e8de06ce1c2b0c09ca752a46
Andrei <atrubaa@gmail.com>
2023-03-06 09:46:55 -0800

    add expression

--- Basic_JavaScript_projects/Project1_expressions_alert/JS/JavaScript_File1.js
+++ Basic_JavaScript_projects/Project1_expressions_alert/JS/JavaScript_File1.js
@@ -1,3 +1,3 @@
 var Text = "You\' are fucking cockroach: \"I'll kill you like a freaking dog!\"", Dad="Bobby", Mother="Julia";
 var Text2 = " Sorry, it is a bad day today";
-document.write(Mother);
+document.write(12*5421,3);
