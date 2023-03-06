e454a0250222e1bd0859fc2e36115eb8c34d92e8
Andrei <atrubaa@gmail.com>
2023-03-06 12:33:11 -0800

    another math funcation

--- Basic_JavaScript_projects/Project3_math_operators/JS/main.js
+++ Basic_JavaScript_projects/Project3_math_operators/JS/main.js
@@ -2,3 +2,8 @@ function Addition(){
     var math = 2+2;
     document.getElementById("math").innerHTML="2+2=" + math;
 }
+
+function Substraction(){
+    var subst = 5-2
+    document.getElementById("substract").innerHTML="5-2=" +subst;
+}
