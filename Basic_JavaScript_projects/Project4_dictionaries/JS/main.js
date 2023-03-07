c93bf70b96c3d14b5f60b499e39dec9d510ec74b
Andrei <atrubaa@gmail.com>
2023-03-07 09:15:49 -0800

    add comments

--- Basic_JavaScript_projects/Project4_dictionaries/JS/main.js
+++ Basic_JavaScript_projects/Project4_dictionaries/JS/main.js
@@ -5,6 +5,6 @@ function Dictionary(){
         hometown: "Novosibirsk",
         occupation: "game-developer",
     };
-    delete Dict.hometown;
-    document.getElementById("dictionary").innerHTML=Dict.hometown;
+    delete Dict.hometown; //tried delete keyword to delete KVP for hometown
+    document.getElementById("dictionary").innerHTML=Dict.hometown; //to searchmy dictionary for hometown
 }
