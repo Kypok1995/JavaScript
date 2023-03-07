0184fa8d543086036007578adf14f63d56315098
Andrei <atrubaa@gmail.com>
2023-03-07 10:29:42 -0800

    tried === operator

--- Basic_JavaScript_projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic_JavaScript_projects/Project5_comparisons_type_coercion/JS/main.js
@@ -15,8 +15,18 @@ document.write(-2e310) //to display negative infinity
 document.write(10>5); //to display true
 document.write(5<=4)
 
-console.log(5+13);
+console.log(5+13); // to type smth in the console
 
 console.log(10<5);
 
-document.write((10+2)==13);
+document.write((10+2)==13); // equality operator, return true if operands are equal
+
+var A = 10;
+var B = 10;
+var C = "15";
+var D = "10";
+var E = 12;
+document.write(A===B); // display true, as they have same values and types
+document.write(A===C); //display false as they have different values and types
+document.write(A===D); //display false as it is different types
+document.write(A===E); // display false, as values are different
