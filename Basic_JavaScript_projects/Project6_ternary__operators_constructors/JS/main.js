4f043a1ede171f54a93aa1d8513c657f2dd755e8
Andrei <atrubaa@gmail.com>
2023-03-07 13:09:13 -0800

    added to vote function

--- Basic_JavaScript_projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic_JavaScript_projects/Project6_ternary__operators_constructors/JS/main.js
@@ -2,5 +2,12 @@ function Ride_Function(){
     var Height, Can_ride;
     Height = document.getElementById("Height").value;
     Can_ride = (Height < 52) ? "you are too short":"you are tall enough";
-    document.getElementById("Ride").innerHTML=Can_ride + "to ride";
+    document.getElementById("Ride").innerHTML=Can_ride + " to ride";
+}
+
+unction Vote_Function(){
+    var Age, Can_vote;
+    Age = document.getElementById("Age").value;
+    Can_vote = (Age < 18) ? "you are too young ":"you are old enough ";
+    document.getElementById("Vote").innerHTML=Can_vote + " to vote";
 }
