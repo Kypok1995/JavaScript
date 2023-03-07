1c6bdbb7f2e2f3628cefb0b3f6cb601ea5dd3dee
Andrei <atrubaa@gmail.com>
2023-03-07 09:45:30 -0800

    tried isNan method

--- Basic_JavaScript_projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic_JavaScript_projects/Project5_comparisons_type_coercion/JS/main.js
@@ -4,5 +4,6 @@ document.write(typeof"Andrei");
 
 function NotANumber(){
     document.getElementById("test").innerHTML=(0/0);
-    document.getElementById("test").innerHTML=isNaN("Fifteen");
+    document.getElementById("test1").innerHTML=isNaN("Fifteen");
+    document.getElementById("test1").innerHTML=isNaN("106");
 }
