f30c64d7ffffa1c028b099cb8c34f99bc9c9264a
Andrei <atrubaa@gmail.com>
2023-03-07 09:46:10 -0800

    fixed bug

--- Basic_JavaScript_projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic_JavaScript_projects/Project5_comparisons_type_coercion/JS/main.js
@@ -5,5 +5,5 @@ document.write(typeof"Andrei");
 function NotANumber(){
     document.getElementById("test").innerHTML=(0/0);
     document.getElementById("test1").innerHTML=isNaN("Fifteen");
-    document.getElementById("test1").innerHTML=isNaN("106");
+    document.getElementById("test2").innerHTML=isNaN("106");
 }
