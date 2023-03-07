f0ef086d741d81a12980b1e40a5d768be563c5f2
Andrei <atrubaa@gmail.com>
2023-03-07 13:22:39 -0800

    added comments

--- Basic_JavaScript_projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic_JavaScript_projects/Project6_ternary__operators_constructors/JS/main.js
@@ -1,12 +1,12 @@
 function Ride_Function(){
-    var Height, Can_ride;
-    Height = document.getElementById("Height").value;
-    Can_ride = (Height < 52) ? "you are too short":"you are tall enough";
-    document.getElementById("Ride").innerHTML=Can_ride + " to ride";
+    var Height, Can_ride; // declare a value
+    Height = document.getElementById("Height").value; // assign a variable height a value of user's input
+    Can_ride = (Height < 52) ? "you are too short":"you are tall enough"; // assign Can_ride variable one of two possible values, based on ternary operator ?
+    document.getElementById("Ride").innerHTML=Can_ride + " to ride"; // print out result into blank paragraph in HTML
 }
 
 
-function Vote_Function(){
+function Vote_Function(){ // analog of ride_function, checks if user is older than 18 or not, using ternary operator ?
     var Age, Can_vote;
     Age = document.getElementById("Age").value;
     Can_vote = (Age < 18) ? "you are too young ":"you are old enough ";
