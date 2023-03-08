74a645a9372bc0fd9cb2bfec19237ad94358c8aa
Andrei <atrubaa@gmail.com>
2023-03-08 10:35:35 -0800

    added else if condition

--- Basic_JavaScript_projects/Project7_scope_time_function/JS/main.js
+++ Basic_JavaScript_projects/Project7_scope_time_function/JS/main.js
@@ -43,3 +43,20 @@ function Alko_function(){
         document.getElementById("Alko").innerHTML="You are too small for vodka, call your dad"
     }
 }
+
+function TimeFunction(){
+    var Time = new Date().getHours();//assign current hour to variable
+    var Reply; // variable to hold a reply 
+    if(Time<12 == Time>0){
+        Reply="It is morning time!"
+    }
+
+    else if(Time >=12 == Time<18){
+        Reply="It is afternoon!"
+    }
+
+    else{
+        Reply="It is evening time!"
+    }
+    document.getElementById("timeofDay").innerHTML=Reply;
+}
